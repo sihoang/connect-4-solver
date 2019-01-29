7c1bd5571a11991389cfcb776d3f6ca545b105e6
Hoang Nguyen <hoang@wetrust.io>
2019-01-28 22:34:47 -0800

    initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "plugins": ["prettier"],
+  "rules": {
+    "prettier/prettier": ["error", {"trailingComma": "all", "singleQuote": true}],
+    "no-console": "off",
+    "no-restricted-syntax": "off",
+    "no-await-in-loop": "off",
+    "import/no-dynamic-require": "off",
+  }
+};
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import '@babel/polyfill';
+
+async function main() {
+  console.log('Connect 4 Solver');
+}
+
+main();
