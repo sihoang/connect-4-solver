c472a0e80cb451955d8e37642ab444f48fc601f2
Hoang Nguyen <hoang@wetrust.io>
2019-01-28 22:49:25 -0800

    add user input

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import '@babel/polyfill';
+import readline from 'readline';
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const readUserInput = (question) => {
+  return new Promise((resolve, reject) => {
+    rl.question(question, answer => {
+      if (answer === 'quit') {
+        reject('exiting');
+      } else {
+        resolve(answer)
+      }
+    });
+  })
+};
 async function main() {
   console.log('Connect 4 Solver');
 }
 
-main();
+main().finally(() => {
+  rl.close();
+});
